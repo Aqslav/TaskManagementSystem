742135c8f9d4b0e7ef4cabfe0c451c7830094e56
Aqslav <antonio.mudryj@gmail.com>
2026-04-09 01:01:12 +0300

    Added print_tasks()

--- manager.py
+++ manager.py
@@ -4,6 +4,13 @@ class manager:
 
     def add_task(self, task):
         self.tasks.append(task)
-        
+
     def remove_task(self, task):
         self.tasks.remove(task)
+    
+    def print_tasks(self):
+        for i in range(len(self.tasks)):
+            print(f"{i+1}. {self.tasks[i].name}")
+            print(f"   Description: {self.tasks[i].description}")
+            print(f"   Time: {self.tasks[i].time}")
+            print(f"   Priority: {self.tasks[i].priority}")
