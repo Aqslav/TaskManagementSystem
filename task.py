25e0e1823d06489209d344ea390a539d175b30af
Aqslav <antonio.mudryj@gmail.com>
2026-04-09 00:52:41 +0300

    Added task class and init

---
+++ task.py
@@ -0,0 +1,8 @@
+class task:
+    def __init__(self, name, description, time, priority):
+        self.name = name
+        self.description = description
+        self.time = time
+        self.priority = priority
+
+    
