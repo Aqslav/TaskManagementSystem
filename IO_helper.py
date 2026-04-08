3b78fdc8cd8ae818180d5f2de1745f5af7325cea
Aqslav <antonio.mudryj@gmail.com>
2026-04-09 01:14:31 +0300

    Added load method

--- IO_helper.py
+++ IO_helper.py
@@ -6,3 +6,8 @@ class IO_helper:
     def save(data, filename):
         with open(filename, 'w') as f:
             json.dump(data, f)
+    
+    @staticmethod
+    def load(filename):
+        with open(filename, 'r') as f:
+            return json.load(f)
