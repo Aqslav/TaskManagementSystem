4a0104d3a5826e2aec9bc201d1af04561955d4ca
Aqslav <antonio.mudryj@gmail.com>
2026-04-09 01:33:16 +0300

    bug fix

--- IO_helper.py
+++ IO_helper.py
@@ -4,10 +4,19 @@ import json
 class IO_helper:
     @staticmethod
     def save(data, filename):
-        with open(filename, 'w') as f:
-            json.dump(data, f)
-    
+        arr = []
+        for item in data:
+            if hasattr(item, "__dict__"):
+                arr.append(item.__dict__)
+            elif isinstance(item, dict):
+                arr.append(item)
+            else:
+                raise TypeError(f"Unsupported item type: {type(item).__name__}")
+
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(arr, f, ensure_ascii=False, indent=2)
+
     @staticmethod
     def load(filename):
-        with open(filename, 'r') as f:
+        with open(filename, "r", encoding="utf-8") as f:
             return json.load(f)
